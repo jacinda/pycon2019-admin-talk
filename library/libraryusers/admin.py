7675b88c199a65852f4ef7435f0ea16e0a474ea8
Jacinda Shelly <jshelly@doctorondemand.com>
2015-09-07 00:14:35 -0500

    Added user example code

---
+++ library/libraryusers/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import LibraryUser
+#from stacks.admin import LoanedBookInline
+
+@admin.register(LibraryUser)
+class LibraryUserAdmin(UserAdmin):
+    pass
+
+    #inlines = (LoanedBookInline,)
+
+    #def get_fieldsets(self, request, obj=None):
+    #    return super(LibraryUserAdmin, self).get_fieldsets(request, obj) + (
+    #            ('Custom Fields', {'fields': ('birthdate', 'gender')}),
+    #    )
+
+    #def get_formsets_with_inlines(self, request, obj=None):
+    #    for inline in self.get_inline_instances(request, obj):
+    #        if isinstance(inline, LoanedBookInline) and obj is None:
+    #            continue
+    #        yield inline.get_formset(request, obj), inline
