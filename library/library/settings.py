8a98979939519b48772c8059ae6c84d0ecc4a8bd
Jacinda Shelly <jacinda.shelly@gmail.com>
2019-05-03 19:00:41 -0400

    Add INTERNAL_IPS

--- library/library/settings.py
+++ library/library/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "debug_toolbar",
 ]
 
+INTERNAL_IPS = ['127.0.0.1',]
+
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
